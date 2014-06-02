a0c76096a4c692e51842faab90446bbc8825f34d
Joe Stanton <joe.stanton@red-badger.com>
2014-06-02 20:56:35 +0100

    Added docco based site for now

---
+++ index.swift
@@ -0,0 +1,22 @@
+// Method Calls - C Style
+
+println("Hello World")
+
+// Constants (immutable) defined with let
+
+let boolVariable = false
+let maximumAttempts = 10
+let currentTemperature = 18.57
+
+// Unicode identifiers
+let π = 3.14159
+
+// Variables defined with "var"
+var canChange = false
+canChange = true
+
+// Optional Type Annotations
+var Bool: canChange
+
+// Define multiple constants/variables on one line
+var x = 0.0, y = 0.0
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var url = require('url');
 var express = require('express');
+//var compress = require('compress');
+var static = require('serve-static');
 var app = express();
-app.use(express.logger('dev'));
 
-app.use(express.compress());
+//app.use(compress());
 app.use(express.static('public'));
-app.set('view engine', 'jade')
-app.set('views', __dirname)
 
 app.listen(process.env.PORT || 5000);
